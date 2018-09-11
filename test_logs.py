ccae7ef2ca4be38f1beeab44a0af4ac9cc22826e
¨KrisKorrel¨ <kriskorrel@gmail.com>
2018-09-11 13:48:52 +0200

    fix print of std

--- test_logs.py
+++ test_logs.py
@@ -131,7 +131,7 @@ for i, model in enumerate(natural_sort(mean_avg)):
     datadict = OrderedDict(sorted(mean_avg[model].items(), key=lambda x : x[0], reverse=False))
     datadict_std = OrderedDict(sorted(std_avg[model].items(), key=lambda x: x[0], reverse=False))
     # print('%s:\t%s' % (model, '\t'.join(['%s %.4f' % (d, datadict[d]) for d in datadict])))
-    print('%s:\t%s' % (model, '\t'.join(['%.4f' % (datadict[d]) for d in datadict])))
+    # print('%s:\t%s' % (model, '\t'.join(['%.4f' % (datadict[d]) for d in datadict])))
     print('%s:\t%s' % (model, '\t'.join(['%.4f ($\\pm$%.2f)' % (datadict[d], datadict_std[d]) for d in datadict])))
 
 def k_restrict(input_str):
