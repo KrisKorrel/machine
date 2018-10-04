38746c8980a71aa110077a2056de7ca1863151be
¨KrisKorrel¨ <kriskorrel@gmail.com>
2018-10-04 13:43:26 +0200

    change norm exp from lookup to sr

--- exp_norm.sh
+++ exp_norm.sh
@@ -27,24 +27,19 @@ DROPOUT_ENC_DEC=0
 RNN_CELL=gru
 FULL_ATTENTION_FOCUS=no
 
-DATA_PATH="../machine-tasks/LookupTables/lookup-3bit/samples/sample1"
-TRAIN_PATH="${DATA_PATH}/train.tsv"
-VALIDATION_PATH="${DATA_PATH}/validation.tsv"
-TEST1_PATH="${DATA_PATH}/heldout_compositions.tsv"
-TEST2_PATH="${DATA_PATH}/heldout_inputs.tsv"
-TEST3_PATH="${DATA_PATH}/heldout_tables.tsv"
-TEST4_PATH="${DATA_PATH}/new_compositions.tsv"
-TEST5_PATH="${DATA_PATH}/longer_compositions_incremental.tsv"
-TEST6_PATH="${DATA_PATH}/longer_compositions_new.tsv"
-TEST7_PATH="${DATA_PATH}/longer_compositions_seen.tsv"
-TEST8_PATH="${DATA_PATH}/heldout_tables_sn.tsv"
-TEST9_PATH="${DATA_PATH}/heldout_tables_ns.tsv"
-MONITOR_DATA="${VALIDATION_PATH} ${TEST1_PATH} ${TEST2_PATH} ${TEST3_PATH} ${TEST4_PATH} ${TEST5_PATH} ${TEST6_PATH} ${TEST7_PATH} ${TEST8_PATH} ${TEST9_PATH}"
+DATA_PATH="../machine-tasks/SymbolRewriting"
+TRAIN_PATH="${DATA_PATH}/grammar_std.train.full.tsv"
+VALIDATION_PATH="${DATA_PATH}/grammar.val.tsv"
+TEST1_PATH="${DATA_PATH}/grammar_std.tst.full.tsv"
+TEST2_PATH="${DATA_PATH}/grammar_repeat.tst.full.tsv"
+TEST3_PATH="${DATA_PATH}/grammar_short.tst.full.tsv"
+TEST4_PATH="${DATA_PATH}/grammar_long.tst.full.tsv"
+MONITOR_DATA="${VALIDATION_PATH} ${TEST1_PATH} ${TEST2_PATH} ${TEST3_PATH} ${TEST4_PATH}"
 
-TF=0
-EPOCHS=100
-BATCH_SIZE=1
-METRICS="word_acc seq_acc"
+TF=1
+EPOCHS=10
+BATCH_SIZE=128
+METRICS="word_acc seq_acc sym_rwr_acc"
 
 # MODEL SPECIFIC PARAMETERS
 MODEL_TYPE="baseline"
@@ -72,7 +67,7 @@ for MODEL in baseline learned oracle; do
         EXTRAARG=""
     fi
 
-    for RUN in 1 2 3; do
+    for RUN in 1; do
         EXPT_DIR="norm_${MODEL}_run_${RUN}"
         python3 train_model.py --train $TRAIN_PATH --dev $VALIDATION_PATH --monitor $MONITOR_DATA --metrics $METRICS --output_dir $EXPT_DIR --epochs $EPOCHS --rnn_cell $RNN_CELL --embedding_size $EMB_SIZE --hidden_size $HIDDEN_SIZE --dropout_p_encoder $DROPOUT --dropout_p_decoder $DROPOUT --teacher_forcing_ratio $TF --attention $ATTENTION --attention_method $ATTN_METHOD --batch_size $BATCH_SIZE --eval_batch_size $EVAL_BATCH_SIZE --save_every $SAVE_EVERY --print_every $PRINT_EVERY --write-logs "${EXPT_DIR}_LOG" --cuda_device $CUDA --understander_train_method supervised --sample_train $SAMPLE_TRAIN --sample_infer $SAMPLE_INFER --initial_temperature $INIT_TEMP --learn_temperature $LEARN_TEMP --init_exec_dec_with $INIT_EXEC_DEC_WITH --attn_keys $ATTN_KEYS --attn_vals $ATTN_VALS --dropout_enc_dec $DROPOUT_ENC_DEC --model_type $MODEL_TYPE --full_attention_focus $FULL_ATTENTION_FOCUS $EXTRAARG > "${EXPT_DIR}_out.txt" 2> "${EXPT_DIR}_err.txt" &
         
