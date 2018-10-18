1c261663592235b24f80ffa953f2003542e301d5
¨KrisKorrel¨ <kriskorrel@gmail.com>
2018-10-18 18:37:13 +0200

    reimplement sparsemax

--- seq2seq/util/sparsemax.py
+++ seq2seq/util/sparsemax.py
@@ -1,25 +1,73 @@
-"""Sparsemax activation function wrapper.
+"""Sparsemax activation function.
 
-From https://github.com/vene/sparse-structured-attention
-Edited slightly because it did not work out of the box.
-
-SparsemaxFunction is the actual function. Sparsemax is the module that calls the function.
-Otherwise you would have to call sparsemax.forward(x) instead of sparsemax(x)
+Pytorch implementation of Sparsemax function from:
+-- "From Softmax to Sparsemax: A Sparse Model of Attention and Multi-Label Classification"
+-- André F. T. Martins, Ramón Fernandez Astudillo (http://arxiv.org/abs/1602.02068)
 """
 
-from .sparsemax_function import SparsemaxFunction
+from __future__ import division
 
+import torch
 import torch.nn as nn
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 class Sparsemax(nn.Module):
-    """Sparsemax module."""
+    """Sparsemax function."""
 
-    def __init__(self):
-        """Initialize sparsemax function."""
-        super(Sparsemax, self).__init__()
-        self.sparsemax_function = SparsemaxFunction()
+    def forward(self, input):
+        """Forward function.
 
-    def forward(self, x, lengths=None):
-        """Forward progration."""
-        return self.sparsemax_function.forward(x, lengths)
+        Input should 2-dimensional.
+
+        Args:
+            input (torch.Tensor): [batch_size x number_of_logits] Input tensor
+
+        Returns:
+            torch.Tensor: [batch_size x number_of_logits] Output tensor
+
+        """
+        dim = 1
+        number_of_logits = input.size(dim)
+
+        # Translate input by max for numerical stability
+        input = input - torch.max(input, dim=dim, keepdim=True)[0].expand_as(input)
+
+        # Sort input in descending order.
+        # (NOTE: Can be replaced with linear time selection method described here:
+        # http://stanford.edu/~jduchi/projects/DuchiShSiCh08.html)
+        zs = torch.sort(input=input, dim=dim, descending=True)[0]
+        range = torch.range(start=1, end=number_of_logits, device=device).view(1, -1)
+        range = range.expand_as(zs)
+
+        # Determine sparsity of projection
+        bound = 1 + range * zs
+        cumulative_sum_zs = torch.cumsum(zs, dim)
+        is_gt = torch.gt(bound, cumulative_sum_zs).type(input.type())
+        k = torch.max(is_gt * range, dim, keepdim=True)[0]
+
+        # Compute threshold function
+        zs_sparse = is_gt * zs
+
+        # Replace NaNs (caused by manually set -Infs) to 0. Same effect as setting is_gt at those
+        # positions to 0/False
+        zs_sparse[zs_sparse != zs_sparse] = 0
+
+        taus = (torch.sum(zs_sparse, dim, keepdim=True) - 1) / k
+        taus = taus.expand_as(input)
+
+        # Sparsemax
+        self.output = torch.max(torch.zeros_like(input), input - taus)
+
+        return self.output
+
+    def backward(self, grad_output):
+        """Backward function."""
+        dim = 1
+
+        nonzeros = torch.ne(self.output, 0)
+        sum = torch.sum(grad_output * nonzeros, dim=dim) / torch.sum(nonzeros, dim=dim)
+        self.grad_input = nonzeros * (grad_output - sum.expand_as(grad_output))
+
+        return self.grad_input
