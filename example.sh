021cb11288f3301a254f0ab73b0f0f6760312098
¨KrisKorrel¨ <kriskorrel@gmail.com>
2018-11-15 14:04:07 +0100

    Cleanup

--- example.sh
+++ example.sh
@@ -1,18 +1,24 @@
 #! /bin/sh
 
-TRAIN_PATH='./train_parity.tsv'
-DEV_PATH='./test_parity.tsv'
+TRAIN_PATH=test/test_data/train.txt
+DEV_PATH=test/test_data/dev.txt
 EXPT_DIR=example
 
 # set values
-EMB_SIZE=32
-H_SIZE=32
+EMB_SIZE=16
+H_SIZE=64
 N_LAYERS=1
-CELL='lstm'
-EPOCH=1000
-PRINT_EVERY=100
+CELL='gru'
+EPOCH=6
+PRINT_EVERY=10
 TF=0.5
 
 # Start training
 echo "Train model on example data"
-python train_model.py --max_len 100 --train $TRAIN_PATH --output_dir $EXPT_DIR --print_every $PRINT_EVERY --embedding_size $EMB_SIZE --hidden_size $H_SIZE --rnn_cell $CELL --n_layers $N_LAYERS --epoch $EPOCH --print_every $PRINT_EVERY --teacher_forcing $TF --ponder_encoder --ponder_decoder --max_ponder_steps 10 --ponder_epsilon 0.05 --ponder_penalty_scale 0.001 --batch_size 100 --eval_batch_size 1000 --ignore_output_eos
+python train_model.py --train $TRAIN_PATH --output_dir $EXPT_DIR --print_every $PRINT_EVERY --embedding_size $EMB_SIZE --hidden_size $H_SIZE --rnn_cell $CELL --n_layers $N_LAYERS --epoch $EPOCH --print_every $PRINT_EVERY --teacher_forcing $TF --attention 'pre-rnn' --attention_method 'mlp'
+
+echo "\n\nEvaluate model on test data"
+python evaluate.py --checkpoint_path $EXPT_DIR/$(ls -t $EXPT_DIR/ | head -1) --test_data $TRAIN_PATH
+
+echo "\n\nRun in inference mode"
+python infer.py --checkpoint_path $EXPT_DIR/$(ls -t $EXPT_DIR/ | head -1) 
