0ac09966fc23c29c85814c69bdb0f83590921fdd
¨KrisKorrel¨ <kriskorrel@gmail.com>
2018-11-16 10:17:49 +0100

    Cleanup: gumbel

--- machine/util/gumbel.py
+++ machine/util/gumbel.py
@@ -1,26 +1,21 @@
+"""Gumbel-Softmax (with and without Straight-Through Estimator.
+
+based on
+https://github.com/ericjang/gumbel-softmax/blob/3c8584924603869e90ca74ac20a6a03d99a91ef9/Categorical%20VAE.ipynb
+"""
+
 import torch
 from torch.nn.functional import softmax
 
-def _sample_gumbel(shape, eps, out=None):
-    """
-    Sample from Gumbel(0, 1)
 
-    based on
-    https://github.com/ericjang/gumbel-softmax/blob/3c8584924603869e90ca74ac20a6a03d99a91ef9/Categorical%20VAE.ipynb ,
-    (MIT license)
-    """
+def _sample_gumbel(shape, eps, out=None):
+    """Sample from Gumbel(0, 1)."""
     U = out.resize_(shape).uniform_() if out is not None else torch.rand(shape)
     return - torch.log(eps - torch.log(U + eps))
 
 
 def _gumbel_softmax_sample(logits, invalid_action_mask, gumbel, tau, eps):
-    """
-    Draw a sample from the Gumbel-Softmax distribution
-
-    based on
-    https://github.com/ericjang/gumbel-softmax/blob/3c8584924603869e90ca74ac20a6a03d99a91ef9/Categorical%20VAE.ipynb
-    (MIT license)
-    """
+    """Draw a sample from the Gumbel-Softmax distribution."""
     dims = logits.dim()
     gumbel_noise = _sample_gumbel(logits.size(), eps=eps, out=logits.data.new())
 
@@ -29,10 +24,12 @@ def _gumbel_softmax_sample(logits, invalid_action_mask, gumbel, tau, eps):
     else:
         y = logits
 
-    # Make temperature same dimensions as y and make certain values non-differentiable by filling their content
-    # Otherwise, pytorch would try to differentiate, y would contain -inf values, making y/tau produce NaN gradients
-    # We need to use contiguous to make sure that masked_fill_ does not fill all copied values, but only the ones indicated
-    # by the mask.
+    # Make temperature same dimensions as y and
+    # make certain values non-differentiable by filling their content.
+    # Otherwise, pytorch would try to differentiate,
+    # y would contain -inf values, making y/tau produce NaN gradients
+    # We need to use contiguous to make sure that masked_fill_ does not fill all copied values,
+    # but only the ones indicated by the mask.
     # We expect here that y has already been masked with -inf with the same mask
     tau = tau.expand_as(y).contiguous()
     tau.masked_fill_(mask=invalid_action_mask, value=1)
@@ -41,8 +38,8 @@ def _gumbel_softmax_sample(logits, invalid_action_mask, gumbel, tau, eps):
 
 
 def gumbel_softmax(logits, invalid_action_mask, tau, gumbel, hard, eps):
-    """
-    Sample from the Gumbel-Softmax distribution and optionally discretize.
+    """Sample from the Gumbel-Softmax distribution and optionally discretize.
+
     Args:
       logits: [batch_size, n_class] unnormalized log-probs
       tau: non-negative scalar temperature
@@ -54,10 +51,6 @@ def gumbel_softmax(logits, invalid_action_mask, tau, gumbel, hard, eps):
 
     Constraints:
     - this implementation only works on batch_size x num_features tensor for now
-
-    based on
-    https://github.com/ericjang/gumbel-softmax/blob/3c8584924603869e90ca74ac20a6a03d99a91ef9/Categorical%20VAE.ipynb ,
-    (MIT license)
     """
     shape = logits.size()
     assert len(shape) == 2
